9367f4917715a6cd026b49bba15ed206973a78f1
Jhiro Faran <jhiraf07@gmail.com>
2024-09-23 18:18:14 +0700

    [CREATE] new entity customer

---
+++ src/main/java/com/nexura/pointofsales/demo/entity/Customer.java
@@ -0,0 +1,13 @@
+package com.nexura.pointofsales.demo.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "c_customer")
+public class Customer {
+}
