02cfc8ca5f83611dc65cca239b4a4873b9f39032
Jhiro Faran <jhiraf07@gmail.com>
2024-09-23 17:49:25 +0700

    [NEW] declare project

---
+++ src/test/java/com/nexura/pointofsales/demo/PointOfSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexura.pointofsales.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
